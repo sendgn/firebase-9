c8676603f77fc10b8ab3c90b48fa6925089d3d05
Aleksandr Polozhenskii <polozhenskii@gmail.com>
2023-09-17 13:45:57 +0300

    Fetch documents from firestore

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import { initializeApp } from 'firebase/app';
+import {
+  getFirestore, collection, getDocs
+} from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: "AIzaSyB-qOtsmwlOtOUNHQbx6rf_enkxzc2ZlHs",
@@ -9,5 +12,28 @@ const firebaseConfig = {
   appId: "1:587458817876:web:27f3fded09a57f0a1a22c1"
 };
 
-// Connect our app to firebase backend using the config
+// Init firebase app
+// connect our app to firebase backend using the config
 initializeApp(firebaseConfig);
+
+// Init services
+// it represents our data base connection, and anytime we do smth
+// with the database like reach out to get data we're gonna use this constant
+const db = getFirestore();
+
+// Collection ref
+// get an access to a specific collection (in our case - books)
+const colRef = collection(db, 'books');
+
+// Get collection data
+getDocs(colRef)
+  .then((snapshot) => {
+    let books = [];
+    snapshot.docs.forEach((doc) => {
+      books.push({ ...doc.data(), id: doc.id });
+    });
+    console.log(books);
+  })
+  .catch (err => {
+    console.log(err.message)
+  })
