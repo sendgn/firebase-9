f013ca16b1ac19f7a692149c6f2ea76a89eb5443
Aleksandr Polozhenskii <polozhenskii@gmail.com>
2023-09-17 18:53:44 +0300

    Use firestore queries

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import { initializeApp } from 'firebase/app';
 import {
   getFirestore, collection, onSnapshot,
-  addDoc, deleteDoc, doc
+  addDoc, deleteDoc, doc,
+  query, where
 } from 'firebase/firestore'
 
 const firebaseConfig = {
@@ -14,22 +15,26 @@ const firebaseConfig = {
 };
 
 // Init firebase app
+// ----------------------
 // connect our app to firebase backend using the config
 initializeApp(firebaseConfig);
 
 // Init services
+// ---------------
 // it represents our data base connection, and anytime we do smth
 // with the database like reach out to get data we're gonna use this constant
 const db = getFirestore();
 
 // Collection ref
+// ----------------
 // get an access to a specific collection (in our case - books)
 const colRef = collection(db, 'books');
 
 // Real time collection data
-// Set a subscription (or realtime listener) to a firestore collection
+// ---------------------------
+// Set a subscription (or real time listener) to a firestore collection
 // to apply changes on a webpage without refreshing it
-// -----------------------------------------------------------
+// ---------------------------------------------------------
 // The second argument is a callback that fires every time
 // there's a change in the collection and it send us back a new snapshot
 // and also once initially as well
@@ -38,6 +43,27 @@ onSnapshot(colRef, (snapshot) => {
   snapshot.docs.forEach((doc) => {
     books.push({ ...doc.data(), id: doc.id });
   });
+  console.log('All books:');
+  console.log(books);
+});
+
+// Firestore queries
+const q = query(colRef, where('author', '==', 'Alexandre Dumas'));
+
+// We're gonna get real time data whenever the data changes
+// that includes the 'author' property to be 'Alexandre Dumas'
+// -------------------------------------------------------------
+// So now if we add a new book where the 'author' is 'Alexandre Dumas'
+// that will fire this function right here where we get a new snapshot
+// ---------------------------------------------------------------------
+// If we add a new book where the 'author' is NOT 'Alexandre Dumas'
+// then we're NOT gonna get a new snapshot
+onSnapshot(q, (snapshot) => {
+  let books = [];
+  snapshot.docs.forEach((doc) => {
+    books.push({ ...doc.data(), id: doc.id });
+  });
+  console.log('Alexandre Dumas\' books:');
   console.log(books);
 });
 
